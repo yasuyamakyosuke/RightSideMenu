be227b0cf9da090c9b9d60b26844c0064be40895
泰山恭輔 <>
2023-07-18 22:30:23 +0900

    first

---
+++ RightSideMenu/RightSideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightSideMenuApp.swift
+//  RightSideMenu
+//
+//  Created by 泰山恭輔 on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct RightSideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
